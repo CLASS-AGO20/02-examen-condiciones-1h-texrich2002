4f1d6c5fca1282c534c6d3e886da880f43270af7
texrich2002 <60866652+texrich2002@users.noreply.github.com>
2020-11-26 15:09:29 -0600

    funcion convertir pesos

--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
+const DOLAR = 20.5;
+const EURO = 22.7;
 export default class App {
-
+    convertir(pesos){
+        let resultado;
+        if(pesos<2000){
+            resultado = pesos/DOLAR;
+        }
+        else if(pesos >= 2000){
+            resultado = pesos/EURO;
+        }
+        return resultado.toFixed(2);
+    }
 }
 
 let app = new App();
+console.log("Funcion convertir pesos: ");
+console.log(app.convertir(1850),"Dolares");
+console.log(app.convertir(2000),"Pesos");
